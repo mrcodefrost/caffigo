27593083b8a2d411bfb87038890fe4cccb2dea4c
mrcodefrost <mr.codefrost@gmail.com>
2025-03-03 18:37:54 +0530

    auth controller setup

--- lib/src/auth/controller/auth_controller.dart
+++ lib/src/auth/controller/auth_controller.dart
@@ -1,3 +1,39 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController {}
+class AuthController extends GetxController {
+  // ========= CONTROLLERS ========= //
+
+  final TextEditingController pinController = TextEditingController();
+
+  // ========= VARIABLES ========= //
+
+  RxString firebaseToken = ''.obs;
+  bool allowLogin = false;
+  RxBool obscureText = true.obs;
+
+  // ========== STATES ========== //
+
+  @override
+  void dispose() {
+    pinController.dispose();
+    super.dispose();
+  }
+
+  // ========== UI Functions ========== //
+
+  void clearAllControllers() {
+    // email.clear();
+    // password.clear();
+    // pin.clear();
+    // resetPasswordEmail.clear();
+  }
+
+  void clearAllControllersExceptEmail() {
+    // password.clear();
+    // pin.clear();
+    // resetPasswordEmail.clear();
+  }
+
+// ========== APIs Functions ========== //
+}
