60c0e349bbe89c2ab581c4aa6a23329677816a8a
mrcodefrost <mr.codefrost@gmail.com>
2025-03-05 18:42:28 +0530

    colors updated

--- lib/utils/color_constant.dart
+++ lib/utils/color_constant.dart
@@ -3,14 +3,17 @@ import 'dart:ui';
 class TextColor {
   static const Color darkBlue = Color(0xff181D2D);
   static const Color blue = Color(0xff324A59);
+  static const Color lightBlue = Color(0xff001833);
   static const Color lightGrey = Color(0xffAAAAAA);
   static const Color grey = Color(0xffC1C7D0);
+  static const Color paleGrey = Color(0xffD8D8D8);
   static const Color brightBlue = Color(0xff007AFF);
   static const Color white = Color(0xffFFFFFF);
 }
 
 class IconColor {
   static const Color lightBlue = Color(0xff001833);
+  static const Color blue = Color(0xff324A59);
   static const Color black = Color(0xff000000);
   static const Color red = Color(0xffFF3030);
   static const Color paleGrey = Color(0xffD8D8D8);
@@ -25,6 +28,8 @@ class DividerColor {
 
 class ButtonColor {
   static const Color blue = Color(0xff324A59);
+  static const Color lameBlue = Color(0xff476374);
+  static const Color paleGrey = Color(0xffD8D8D8);
   static const Color white = Color(0xffFFFFFF);
   static const Color gradientPurple = Color(0xffC58BF2);
   static const Color gradientPink = Color(0xffEEA4CE);
