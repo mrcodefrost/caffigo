3f2719f66c193b9c2ebb1b29589ed447209b985d
mrcodefrost <mr.codefrost@gmail.com>
2025-03-05 18:43:09 +0530

    menu screen done

--- lib/src/menu/view/screens/menu_screen.dart
+++ lib/src/menu/view/screens/menu_screen.dart
@@ -13,75 +13,138 @@ class MenuScreen extends StatelessWidget {
         child: Column(
           spacing: 10,
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Welcome !'),
-                      Text('Frost'),
-                    ],
-                  ),
-                  Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.shopping_cart_outlined),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.person_outline),
-                  ),
-                ],
-              ),
+            MenuAppbar(
+              name: 'Frost',
+              profileOnPressed: () {},
+              cartOnPressed: () {},
             ),
             Expanded(
-                child: Container(
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: BackgroundColor.blue,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+              child: Container(
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: BackgroundColor.blue,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20),
-                    Text(
-                      'Select your items',
-                      style: context.textTheme.bodyLarge!.copyWith(
-                        color: TextColor.white,
-                        fontWeight: FontWeight.w500,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 20),
+                      Text(
+                        'Select your items',
+                        style: context.textTheme.bodyLarge!.copyWith(
+                          color: TextColor.paleGrey,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 25),
-                    ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 150,
-                          width: 150,
-                          // color: BackgroundColor.lighterBlue,
-                          child: Column(
-                            children: [
-                              Image.asset(AppImages.mugCoffee),
-                              Text('Americano'),
-                            ],
+                      const SizedBox(height: 25),
+                      Expanded(
+                        child: GridView.builder(
+                          itemCount: 10,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, // 2 items per row
+                            crossAxisSpacing: 16, // Horizontal space
+                            mainAxisSpacing: 16, // Vertical space
+                            childAspectRatio: 0.9, // Adjust based on design
                           ),
-                        );
-                      },
-                    )
-                  ],
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: BackgroundColor.lighterBlue,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    AppImages.mugCoffee,
+                                    height: 90, // Adjust size
+                                    fit: BoxFit.cover,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    'Americano',
+                                    style:
+                                        context.textTheme.bodyMedium!.copyWith(
+                                      color: TextColor.lightBlue,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            )),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MenuAppbar extends StatelessWidget {
+  final String name;
+  final void Function() cartOnPressed;
+  final void Function() profileOnPressed;
+
+  const MenuAppbar({
+    super.key,
+    required this.cartOnPressed,
+    required this.profileOnPressed,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 5,
+            children: [
+              Text(
+                'Welcome !',
+                style: context.textTheme.bodyLarge!.copyWith(
+                  color: TextColor.paleGrey,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                name,
+                style: context.textTheme.bodyLarge!.copyWith(
+                  color: TextColor.lightBlue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: cartOnPressed,
+            icon: Icon(Icons.shopping_cart_outlined),
+            iconSize: 26,
+          ),
+          IconButton(
+            onPressed: profileOnPressed,
+            icon: Icon(Icons.person_outline),
+            iconSize: 26,
+          ),
+        ],
       ),
     );
   }
