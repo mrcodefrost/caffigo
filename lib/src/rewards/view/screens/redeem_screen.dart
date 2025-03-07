49bfdf5813c199959f4200190119f123a4cb57de
mrcodefrost <mr.codefrost@gmail.com>
2025-03-07 18:47:45 +0530

    primary button added

--- lib/src/rewards/view/screens/redeem_screen.dart
+++ lib/src/rewards/view/screens/redeem_screen.dart
@@ -1,4 +1,5 @@
 import 'package:caffigo/src/common/widgets/custom_app_bar.dart';
+import 'package:caffigo/src/common/widgets/primary_rounded_button.dart';
 import 'package:caffigo/utils/app_constants.dart';
 import 'package:caffigo/utils/color_constant.dart';
 import 'package:flutter/material.dart';
@@ -16,11 +17,12 @@ class RedeemScreen extends StatelessWidget {
         itemCount: 5,
         itemBuilder: (context, index) {
           return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Image.asset(AppImages.mugCoffee, height: 60, fit: BoxFit.cover),
+                Image.asset(AppImages.mugCoffee, height: 58, fit: BoxFit.cover),
+                const SizedBox(width: 15),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -28,19 +30,21 @@ class RedeemScreen extends StatelessWidget {
                       'Latte',
                       style: context.textTheme.bodyLarge!.copyWith(
                         color: TextColor.blue,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                     Text(
-                      'Valid until: 12/12/2023',
+                      'Valid until 12/12/2023',
                       style: context.textTheme.bodySmall!.copyWith(
-                        color: TextColor.paleGrey,
+                        color: TextColor.lightGrey,
                       ),
                     ),
                   ],
                 ),
-                ElevatedButton(
+                Spacer(),
+                PrimaryRoundedButton(
+                  text: '1240 pts',
                   onPressed: () {},
-                  child: Text('1350 Pts'),
                 ),
               ],
             ),
