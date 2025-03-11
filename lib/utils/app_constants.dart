7bf85afb0333b5d8fd3c01fc769d789c1ccfeb09
mrcodefrost <mr.codefrost@gmail.com>
2025-03-11 17:54:29 +0530

    app constants updated

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,12 +1,16 @@
-class AppDimensions {}
+class AppDimensions {
+  static const double horizontalPadding = 25.0;
+  static const double nestedHorizontalPadding = 10.0;
+}
 
 class AppStrings {}
 
 class AppImages {
   static const String logo = 'assets/images/app_logo.png';
-  static const String mugCoffee = 'assets/images/mug_coffee.png';
+  static const String mugCoffee = 'assets/images/mug_coffee.jpg';
 }
 
 class AppIcons {
   static const String takeaway = 'assets/icons/take_away_icon.svg';
+  static const String coffeeMug = 'assets/icons/coffee_mug.svg';
 }
